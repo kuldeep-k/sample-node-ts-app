8722fdc906eaee39db85e1b904c04316e5813e32
Kuldeep Kamboj <kuldeepk1979@gmail.com>
2023-01-21 01:22:17 +0530

    Add express typescript project

---
+++ index.ts
@@ -0,0 +1,18 @@
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app: Express = express();
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello and Welcome');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at {port}`);
+});
+
+
